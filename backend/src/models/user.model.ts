612b2c5d39ed480f39bf1dc1f26ac10277d340a1
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-22 19:32:10 +0530

    new: added User model with password hashing and token generation methods

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,122 @@
+import { Document, model, Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY,
+} from "../config/env.js";
+
+interface UserObject extends Document {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  avatar: string;
+  avatarPublicId: string;
+  coverImage: string;
+  coverImagePublicId: string;
+  refreshToken: string;
+  watchHistory: Array<object>;
+  isPasswordCorrect(password: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      require: [true, "Full Name is required"],
+    },
+    username: {
+      type: String,
+      require: [true, "Username is required"],
+      unique: [true, "Username already exists"],
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+      lowercase: true,
+      trim: true,
+      match: [
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+        "Please enter a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      require: [true, "Password is required"],
+      minLength: [8, "Password length should be atleast 8 or greater"],
+      match: [
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/,
+        "Password should contain alteast one uppercase, one lowercase, one digit and one special character (e.g. Pass@123)",
+      ],
+    },
+    avatar: {
+      type: String,
+    },
+    avatarPublicId: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+    },
+    coverImagePublicId: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre<UserObject>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password! = await bcrypt.hash(this.password, 10);
+  return next();
+});
+
+userSchema.methods = {
+  isPasswordCorrect: async function (this: UserObject, password: string) {
+    return bcrypt.compare(password, this.password);
+  },
+
+  generateAccessToken: function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: ACCESS_TOKEN_EXPIRY,
+      }
+    );
+  },
+
+  generateRefreshToken: function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      REFRESH_TOKEN_SECRET,
+      {
+        REFRESH_TOKEN_EXPIRY,
+      }
+    );
+  },
+};
+
+export const User = model<UserObject>("User", userSchema);
