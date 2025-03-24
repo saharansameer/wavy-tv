837dcb0b1e047081810806dd74fc10a434ac3471
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-25 00:41:24 +0530

    new: add Post model

---
+++ backend/src/models/post.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface PostObject extends Document {
+  content: string;
+  owner: Schema.Types.ObjectId;
+  upvotes: number;
+  downvotes: number;
+}
+
+const postSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      match: [
+        /[a-zA-Z0-9]/,
+        "Content can not be empty and must contain a valid letter",
+      ],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    upvotes: {
+      type: Number,
+      default: 1,
+    },
+    downvotes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model<PostObject>("Post", postSchema);
