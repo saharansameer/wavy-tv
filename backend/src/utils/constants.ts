2830a4d9780651afda1d3f66c9b219c683f6d70b
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-25 21:45:43 +0530

    add: added cookie options and HTTP status codes

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,1 +1,17 @@
 export const SIZE_LIMIT = "1mb";
+
+export const cookiesOptions = {
+  httpOnly: true,
+  secure: true,
+};
+
+export const HTTP_STATUS = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
