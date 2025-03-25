6a1a2bc0735355a4c69b4d656eda7cb639fb6e7e
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-25 21:47:26 +0530

    add: import and declaration of auth routes AND feat: add undefinedRoutesHandler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { SIZE_LIMIT } from "./utils/constants.js";
 import { FRONTEND_URL, LOCAL_HOST } from "./config/env.js";
+import { undefinedRoutesHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -17,5 +18,14 @@ app.use(express.json({ limit: SIZE_LIMIT }));
 app.use(express.urlencoded({ extended: true, limit: SIZE_LIMIT }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes import
+import authRoutes from "./routes/auth.routes.js";
+
+// Routes declaration
+app.use("/api/v1/auth", authRoutes);
+
+// Handle Undefined Routes
+app.use(undefinedRoutesHandler);
 
 export default app;
