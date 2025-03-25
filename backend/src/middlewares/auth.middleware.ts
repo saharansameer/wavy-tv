7f1a03714dfc975316860a015ddd313b073f7590
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-25 21:38:23 +0530

    new: added auth middleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/apiError.js";
+import { ACCESS_TOKEN_SECRET } from "../config/env.js";
+import { HTTP_STATUS } from "../utils/constants.js";
+
+export const checkAuth: Middleware = (req, _res, next) => {
+  const token = req.cookies?.accessToken;
+  if (!token) {
+    throw new ApiError({
+      status: HTTP_STATUS.BAD_REQUEST,
+      message: "Token not found, User is not logged-in",
+    });
+  }
+
+  const validateToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
+  if (!validateToken) {
+    throw new ApiError({
+      status: HTTP_STATUS.BAD_REQUEST,
+      message: "Access Token expired",
+    });
+  }
+  console.log("Token Response:", validateToken);
+  console.log("Full URL:", req.originalUrl);
+  console.log("Path:", req.path);
+  console.log("Query Params:", req.query);
+
+  req.user = validateToken;
+
+  next();
+};
