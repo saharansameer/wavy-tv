cea23f3978b1b39b8f82eea19821d3c2b4bbd61f
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-25 21:40:34 +0530

    new: added auth routes

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { errorHandler } from "../middlewares/error.middleware.js";
+import { checkAuth } from "../middlewares/auth.middleware.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  renewTokens,
+} from "../controllers/auth.controller.js";
+
+const router = Router();
+
+// POST - Register new user
+router.route("/signup").post(asyncHandler(registerUser));
+
+// POST - Login user
+router.route("/login").post(asyncHandler(loginUser));
+
+// POST - Logout user
+router.route("/logout").post(asyncHandler(checkAuth), asyncHandler(logoutUser));
+
+// POST - Renew Token
+router.route("/token").post(asyncHandler(renewTokens));
+
+// Error Handler
+router.use(errorHandler);
+
+export default router;
