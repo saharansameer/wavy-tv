20db23d3fc2ff68a84cfb0580ec747cff3968b39
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-25 21:44:20 +0530

    refactor: update asyncHandler to support both Controller and Middleware types

--- backend/src/utils/asyncHandler.ts
+++ backend/src/utils/asyncHandler.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
-export const asyncHandler = (requestHandler: Controller) => {
+export const asyncHandler = (requestHandler: Controller | Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await requestHandler(req, res);
+      await requestHandler(req, res, next);
     } catch (err: unknown) {
       next(err);
     }
