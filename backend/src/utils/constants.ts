f3a3f5286efe244ee996cd82c21ee917e8b7304c
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-26 18:43:25 +0530

    add: added commonly used Response Messages

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -15,3 +15,37 @@ export const HTTP_STATUS = {
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
 };
+
+export const RESPONSE_MESSAGE = {
+  COMMON: {
+    ALL_REQUIRED_FIELDS: "All fields are required",
+    REQ_NOT_FOUND: "No such property found in Request Object",
+  },
+  AUTH: {
+    SIGNUP_SUCCESS: "New user registered successfully",
+    SIGNUP_FAILED: "Failed to register new user",
+    SIGNUP_REQUIRED_FIELDS:
+      "All fields are required (i.e Fullname, Username, Email, Password)",
+    LOGIN_SUCCESS: "User logged-in successfully",
+    LOGIN_FAILED: "Failed to login user",
+    LOGIN_REQUIRED_FIELDS: "Email and Password both are required",
+    INVALID_LOGIN_CREDS: "Invalid Email or Username",
+    INCORRECT_PASSWORD: "Password is incorrect",
+    LOGOUT_SUCCESS: "User logged out successfully",
+    LOGIN_AGAIN: "Session expired. Please log in again",
+  },
+  USER: {
+    NOT_FOUND: "User does not exist",
+    NOT_AUTHORIZED: "User is not authorized",
+    UPDATE_SUCCESS: "User details updated successfully",
+    UPDATE_FAILED: "Failed to update existing user",
+    FETCH_DETAILS_SUCCESS: "User details fetched successfully",
+  },
+  COOKIES: {
+    TOKEN_GENERATED: "New Tokens generated successfully",
+    NOT_FOUND: "Requested cookies does not exist",
+    TOKEN_EXPIRED: "Token is expired or invalid",
+    ACCESS_TOKEN_EXPIRED: "Access token is expired or invalid",
+    REFRESH_TOKEN_EXPIRED: "Refresh token is expired or invalid",
+  },
+};
