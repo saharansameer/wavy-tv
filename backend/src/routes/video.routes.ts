a7e54281d3fc8733a834eeb0c9e5d2830073d4a4
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-03-29 00:42:18 +0530

    new: added video routes

---
+++ backend/src/routes/video.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { errorHandler } from "../middlewares/error.middleware.js";
+import { checkAuth } from "../middlewares/auth.middleware.js";
+import {
+  getAllVideos,
+  getVideoByPublicId,
+  updateVideoDetails,
+  deleteVideo,
+} from "../controllers/video.controller.js";
+
+const router = Router();
+
+// GET - Fetch all videos
+router.route("/").get(asyncHandler(getAllVideos));
+
+// GET - Fetch video by publicId
+// PATCH - Update video details (i.e title, description, publishStatus) by publicId
+// DELETE - Fetch video by publicId
+router
+  .route("/:videoPublicId")
+  .get(asyncHandler(getVideoByPublicId))
+  .patch(checkAuth, asyncHandler(updateVideoDetails))
+  .delete(checkAuth, asyncHandler(deleteVideo));
+
+// Error Handler
+router.use(errorHandler);
+
+export default router;
