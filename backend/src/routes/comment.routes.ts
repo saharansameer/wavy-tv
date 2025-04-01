71e6e77c573c342b19f21086c2d19db48c831a56
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-01 20:23:15 +0530

    new: added routes for Comment controllers

---
+++ backend/src/routes/comment.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { errorHandler } from "../middlewares/error.middleware.js";
+import { checkAuth } from "../middlewares/auth.middleware.js";
+import {
+  addCommentOnVideo,
+  addCommentOnPost,
+  addReplyComment,
+  updateCommentById,
+  deleteCommentById,
+} from "../controllers/comment.controller.js";
+
+const router = Router();
+
+// Check User Auth
+router.use(checkAuth);
+
+// POST - Add Comment on video
+router.route("/video").post(asyncHandler(addCommentOnVideo));
+
+// POST - Add Comment on post
+router.route("/post").post(asyncHandler(addCommentOnPost));
+
+// POST - Add Reply Comment on comment
+router.route("/reply").post(asyncHandler(addReplyComment));
+
+// PATCH - Update Comment by id
+router.route("/update").patch(asyncHandler(updateCommentById));
+
+// DELETE - Delete Comment by id
+router.route("/delete").delete(asyncHandler(deleteCommentById));
+
+// Error Handler
+router.use(errorHandler);
+
+export default router;
