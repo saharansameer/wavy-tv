86e7a7595cb4015f9cdcfefc8a8d0b72a5e9b7f5
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-06 22:50:43 +0530

    feat: tags and keywords extraction from video title and description

--- backend/src/controllers/video.controller.ts
+++ backend/src/controllers/video.controller.ts
@@ -4,6 +4,42 @@ import { Video } from "../models/video.model.js";
 import { HTTP_STATUS, RESPONSE_MESSAGE } from "../utils/constants.js";
 import { trimAndClean } from "../utils/stringUtils.js";
 import { getLoggedInUserId } from "../utils/authUtils.js";
+import { excludedTags } from "../utils/constants.js";
+
+const extractTagsAndKeywords = (title: string, description: string) => {
+  const tagsSet = new Set<string>(); // To store unique tags
+
+  // Convert to lowercase and remove non-alphanumerics
+  const cleanWord = (word: string) => {
+    return word.toLowerCase().replace(/[^\w]/g, "");
+  };
+
+  // Extract tags from title
+  title.split(" ").forEach((word: string) => {
+    const tag = cleanWord(word);
+
+    if (tag && !excludedTags.includes(tag)) {
+      tagsSet.add(tag); // Add cleaned tag if not excluded
+    }
+  });
+
+  // Extract max 5 hashtags from description (if any)
+  if (description !== undefined) {
+    description
+      .split(" ")
+      .filter((word: string) => word.startsWith("#")) // Only hashtags
+      .slice(0, 5) // Limit to first 5
+      .map((word: string) => cleanWord(word)) // Clean them
+      .forEach((tag: string) => {
+        if (tag && !excludedTags.includes(tag)) {
+          tagsSet.add(tag); // Add cleaned hashtag if not excluded
+        }
+      });
+  }
+
+  // Return array of unique and cleaned tags
+  return Array.from(tagsSet);
+};
 
 export const getAllVideos: Controller = async (req, res) => {
   const page = Number(req.query.page as string) || 1;
@@ -188,10 +224,18 @@ export const updateVideoDetails: Controller = async (req, res) => {
     });
   }
 
+  // Extract relevant tags and keywords from Title and Description
+  const tags = extractTagsAndKeywords(trimmedTitle, description);
+
   // Update Video Details (i.e title, description, publishStatus)
   const video = await Video.findOneAndUpdate(
     { publicId: videoPublicId, owner: req.user?._id },
-    { title: trimmedTitle, description, publishStatus },
+    {
+      title: trimmedTitle,
+      description,
+      publishStatus,
+      tags,
+    },
     { new: true, runValidators: true }
   ).select("publicId title description publishStatus");
 
