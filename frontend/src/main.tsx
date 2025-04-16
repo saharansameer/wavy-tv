a5862350f5dd4329bd9c2e1fb6c7283fe6443349
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-16 16:09:31 +0530

    update(main.tsx): configure react-query and react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/app/query/queryClient.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "@/app/routes";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
