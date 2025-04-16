63beb16de0a0ee077db56547a1d831a97e6a92da
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-16 16:01:35 +0530

    feat: add global theme state with persistence

---
+++ frontend/src/components/Theme/Theme.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import useThemeStore from "@/app/store/themeStore";
+
+export default function Theme() {
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    const root = document.querySelector("html");
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+
+      root?.classList.remove("dark", "light");
+      root?.classList.add(systemTheme);
+      return;
+    }
+
+    root?.classList.remove("dark", "light");
+    root?.classList.add(theme);
+  }, [theme]);
+
+  return <></>;
+}
