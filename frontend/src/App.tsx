ead373646b99d50d9b732b32fbef3881b8fde336
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-16 16:08:13 +0530

    update: configure App with Sidebar and Outlet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
+import { Outlet } from "react-router-dom";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar, Header } from "./layout";
+
 function App() {
   return (
-  <div className="bg-background dark:bg-background w-full h-screen">
-    wavyTV Frontend
-  </div>
-);
+    <div className="bg-background dark:bg-background">
+      <div className="w-full py-2 px-2 bg-background dark:bg-background sticky top-0 shadow-sm dark:shadow-[#252525]">
+        <Header />
+      </div>
+
+      <div className="flex flex-row w-full max-w-screen mx-auto">
+        <div>
+          <SidebarProvider>
+            <SidebarTrigger className={"fixed inset-y-4 inset-x-2 cursor-pointer"}/>
+            <AppSidebar />
+          </SidebarProvider>
+        </div>
+        <div className="w-full px-2 py-2">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
