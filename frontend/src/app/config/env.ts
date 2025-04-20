a8e9adbb2bfe31e7f11dde1eb0ffc3ac85436e55
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-20 21:56:21 +0530

    new: setup env variables

---
+++ frontend/src/app/config/env.ts
@@ -0,0 +1,15 @@
+export const CLOUDINARY_CLOUD_NAME = String(
+  import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+);
+export const CLOUDINARY_API_KEY = String(
+  import.meta.env.VITE_CLOUDINARY_API_KEY
+);
+export const CLOUDINARY_API_SECRET = String(
+  import.meta.env.VITE_CLOUDINARY_API_SECRET
+);
+export const CLOUDINARY_UPLOAD_PRESET = String(
+  import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+);
+export const CLOUDINARY_UPLOAD_URL = String(
+  import.meta.env.VITE_CLOUDINARY_UPLOAD_URL
+);
