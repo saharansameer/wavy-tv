5e4573619120e25d38969bae8118ad44a8ac9c9a
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-20 21:57:12 +0530

    add: create NotFound component for undefined routes

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -5,11 +5,14 @@ import {
 } from "react-router-dom";
 import App from "@/App";
 import { Home } from "@/pages";
+import { VideoForm, NotFound } from "@/components";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="" element={<App />}>
       <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<VideoForm />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
---
+++ frontend/src/components/NotFound.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div className="text-4xl font-bold py-20 text-center">oops! <br /> This page no longer exist</div>;
+}
