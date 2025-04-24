81da0af47dec01f95db80e86b29402e240978700
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-24 22:51:02 +0530

    refactor(App): include AuthOverlay in app layout and refined imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
-import { AppSidebar, Header } from "./layout";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar, Header } from "@/layout";
+import { AuthOverlay } from "@/pages";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         </div>
         <div className="w-full px-2 py-2">
           <Outlet />
+          <AuthOverlay />
         </div>
       </div>
     </div>
