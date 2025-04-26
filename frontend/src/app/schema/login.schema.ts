83d6b37c293db1b6cd27ca308ace91610a8f1f5a
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-04-26 16:15:27 +0530

    new: added zod validations for signup and login forms

---
+++ frontend/src/app/schema/login.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const loginFormSchema = z.object({
+  email: z
+    .string()
+    .email()
+    .regex(
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      "Please enter a valid email address"
+    ),
+  password: z.string(),
+});
+
+export type LoginFormSchemaType = z.input<typeof loginFormSchema>;
---
+++ frontend/src/app/schema/signup.schema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const signupFormSchema = z.object({
+  fullName: z
+    .string()
+    .min(1, "Full Name can not be empty")
+    .max(20, "Full Name should not exceed 20 characters"),
+  username: z
+    .string()
+    .min(3, "Username should be at least 3 characters long")
+    .max(20, "Username should not exceed 20 characters"),
+  email: z
+    .string()
+    .email()
+    .regex(
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      "Please enter a valid email address"
+    ),
+  password: z
+    .string()
+    .min(8, "Password should be atleast 8 characters long")
+    .max(128, "Password should not exceed 128 characters")
+    .regex(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/,
+      "Password must include uppercase, lowercase, number, and special character"
+    ),
+});
+
+export type SignupFormSchemaType = z.input<typeof signupFormSchema>;
