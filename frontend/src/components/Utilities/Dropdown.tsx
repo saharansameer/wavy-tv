5b80b8338ffeaa774ccd78c4e3917fe70a55b6db
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-05-01 00:10:12 +0530

    feat: customize shadcn Dropdown with Edit and Delete actions

---
+++ frontend/src/components/Utilities/Dropdown.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui";
+import {
+  AlertOverlay,
+  DrawerOverlay,
+  PostForm,
+  CommentForm,
+} from "@/components";
+import { Ellipsis, Pencil, Trash2 } from "lucide-react";
+
+interface DropdownProps {
+  data: any;
+  entity: "post" | "comment";
+}
+
+export function Dropdown({ data, entity }: DropdownProps) {
+  const isPost = entity === "post";
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"}>
+          <Ellipsis />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-10">
+        <DrawerOverlay
+          trigger={
+            <DropdownMenuItem
+              onSelect={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <Pencil />
+              Edit
+            </DropdownMenuItem>
+          }
+          title={isPost ? "Edit Post" : "Edit Comment"}
+          form={
+            isPost ? (
+              <PostForm mode="patch" data={data} postPublicId={data.publicId} />
+            ) : (
+              <CommentForm mode="patch" data={data} commentId={data._id} />
+            )
+          }
+        />
+
+        <AlertOverlay
+          trigger={
+            <DropdownMenuItem
+              onSelect={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <Trash2 /> Delete
+            </DropdownMenuItem>
+          }
+          entityType={entity}
+          entityId={isPost ? data.publicId : data._id}
+        />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
